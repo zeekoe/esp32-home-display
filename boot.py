387135c96c3d7713a81dca8fdd7cf3f5fc90d9e6
zeekoe <git@ronaldteune.nl>
2023-08-17 21:36:21 +0200

    Updated chart style, tried out touch

--- boot.py
+++ boot.py
@@ -9,8 +9,34 @@ import machine
 from config import user,password
 from espidf import VSPI_HOST,HSPI_HOST
 from ili9XXX import ili9341
+from xpt2046generic import Xpt2046_hw
 disp = ili9341(miso=12, mosi=13, clk=14, cs=15, dc=2, rst=-1, power=-1, backlight=21, backlight_on=1, power_on=0, rot=0x80,spihost=VSPI_HOST, mhz=50, factor=16, hybrid=True, width=240, height=320, invert=False, double_buffer=True, half_duplex=False, initialize=True)
 #touch = xpt2046(cs=33, spihost=HSPI_HOST, mosi=32, miso=39, clk=25, cal_y0 = 423, cal_y1=3948)
+
+
+#define XPT2046_SPI_SCLK 25
+#define XPT2046_SPI_MOSI 32
+#define XPT2046_SPI_MISO 39
+#define XPT2046_SPI_FREQ 2000000
+#define XPT2046_PIN_INT 36
+#define XPT2046_PIN_CS 33
+# Calibration 240x320
+#define XPT2046_MIN_X 349
+#define XPT2046_MAX_X 3859
+#define XPT2046_MIN_Y 247
+#define XPT2046_MAX_Y 3871
+
+
+# spi=machine.SoftSPI(
+#     baudrate=1_000_000, # the chip does not handle more than 2MHz (!)
+#     polarity=0,
+#     phase=0,
+#     sck=machine.Pin(25, machine.Pin.OUT),
+#     mosi=machine.Pin(32, machine.Pin.OUT),
+#     miso=machine.Pin(39, machine.Pin.IN),
+# )
+
+# tsc=Xpt2046_hw(spi=spi,cs=33,rot=1,ranges=((349,3859),(247,3871)))
 
 import network
 print("Connecting to WiFi...")
--- main.py
+++ main.py
@@ -2,28 +2,32 @@ import lvgl as lv
 import json
 import urequests
 import utime
+import machine
+
 
 scr = lv.scr_act()
-the_question_btn = lv.btn(scr)
-the_question_btn.set_size(200,50)
-the_question_label = lv.label(the_question_btn)
-the_question_label.set_text("click me!")
-the_question_label.center()
+scr.set_style_bg_color( lv.color_hex( 0x000000 ), lv.PART.MAIN | lv.STATE.DEFAULT )
+# the_question_btn = lv.btn(scr)
+# the_question_btn.set_size(200,50)
+# the_question_label = lv.label(the_question_btn)
+# the_question_label.set_text("click me!")
+# the_question_label.center()
 
-
-chart = lv.chart(lv.scr_act())
+chart = lv.chart(scr)
 chart.set_size(180,150)
 chart.set_style_bg_color(lv.color_hex(0x000000),0)
 chart.set_style_line_color(lv.color_hex(0x1aec1a),0)
 chart.set_style_border_color(lv.color_hex(0x1aec1a),0)
+chart.set_style_border_color( lv.color_hex( 0x000000 ), lv.PART.MAIN | lv.STATE.DEFAULT )
+chart.set_style_text_color( lv.color_hex( 0x008817 ), lv.PART.TICKS | lv.STATE.DEFAULT )
 
-chart.align_to(the_question_btn, lv.ALIGN.OUT_BOTTOM_RIGHT,40,10)
+chart.align_to(scr, lv.ALIGN.TOP_RIGHT,0,10)
 chart.set_type(lv.chart.TYPE.LINE)
 chart.set_update_mode(lv.chart.UPDATE_MODE.SHIFT)
 chart.set_point_count(60)
 chart.set_div_line_count(0,0)
 
-ser1 = chart.add_series(lv.color_hex(0x00ee00), lv.chart.AXIS.PRIMARY_Y)
+ser1 = chart.add_series(lv.color_hex(0x055800), lv.chart.AXIS.PRIMARY_Y)
 chart.set_range(lv.chart.AXIS.PRIMARY_Y, 0, 1000)
 chart.set_axis_tick(lv.chart.AXIS.PRIMARY_Y, 10, 5, 5, 2, True, 50)
 chart.set_style_size(0, 0, lv.PART.INDICATOR)
