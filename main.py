b51349a9e9ffcbe2afd584d976482b53edb46a48
zeekoe <git@ronaldteune.nl>
2023-10-28 09:01:59 +0200

    warmtepomp

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ chart.set_update_mode(lv.chart.UPDATE_MODE.SHIFT)
 chart.set_point_count(60)
 chart.set_div_line_count(0,0)
 
-ser1 = chart.add_series(lv.color_hex(0x055800), lv.chart.AXIS.PRIMARY_Y)
+ser1 = chart.add_series(lv.color_hex(0x008817), lv.chart.AXIS.PRIMARY_Y)
 chart.set_range(lv.chart.AXIS.PRIMARY_Y, 0, 1000)
 chart.set_axis_tick(lv.chart.AXIS.PRIMARY_Y, 10, 5, 5, 2, True, 50)
 chart.set_style_size(0, 0, lv.PART.INDICATOR)
@@ -37,6 +37,10 @@ url = 'http://192.168.1.120/co2/query.php'
 data = urequests.get(url)
 data_j = json.loads(data.text)
 print(data_j)
+
+ta = lv.textarea(scr)
+ta.align_to(chart,lv.ALIGN.TOP_RIGHT,30,150)
+
 valuebuf = []
 for i in data_j:
     if(type(i['mean']) != None.__class__):
@@ -66,6 +70,29 @@ def update_chart(chart,series):
             chart.set_range(lv.chart.AXIS.PRIMARY_Y, 0, ymax)
             print("Updated yrange to ", ymax)
 
+def update_text(ta):
+    url2 = 'http://192.168.1.120/co2/blackbird.php'
+    data2 = urequests.get(url2)
+    data_j2 = json.loads(data2.text)
+    ta.set_text("Tijd: ")
+    ta.add_text(data_j2['time'])
+    ta.add_text("\nVerm. in: ")
+    ta.add_text(data_j2['Pin'])
+    ta.add_text("\nVerm. uit: ")
+    ta.add_text(data_j2['Pout'])
+    ta.add_text("\nLucht in: ")
+    ta.add_text(data_j2['t_air_in'])
+    ta.add_text("\nLucht uit: ")
+    ta.add_text(data_j2['t_air_out'])
+    ta.add_text("\nWater CV: ")
+    ta.add_text(data_j2['t_water_house_in'])
+    ta.add_text("\nCOP, RPM: ")
+    ta.add_text(data_j2['hCOP'])
+    ta.add_text(", ")
+    ta.add_text(data_j2['rpm'])
+
+
 while True:
     update_chart(chart,ser1)
+    update_text(ta)
     utime.sleep(30)
